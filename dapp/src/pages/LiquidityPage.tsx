24bf836b67b67f4a948a47b38356a3b2f1717603
Crypto-kiki <ejo2123@naver.com>
2025-03-06 16:16:12 +0900

    liquidity

--- dapp/src/pages/LiquidityPage.tsx
+++ dapp/src/pages/LiquidityPage.tsx
@@ -1,12 +1,19 @@
+import AddLiquidity from "@/components/AddLiquidity";
 import ApproveToken from "@/components/ApproveToken";
+import CurrentLiquidity from "@/components/CurrentLiquidity";
 import { OutletContext } from "@/components/Layout";
 import useMetamask from "@/hooks/useMetamask";
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { useOutletContext } from "react-router-dom";
 
 function LiquidityPage() {
-  const { signer, setSigner, tokenAContract, tokenBContract } =
-    useOutletContext<OutletContext>();
+  const {
+    signer,
+    setSigner,
+    tokenAContract,
+    tokenBContract,
+    liquidityPoolContract,
+  } = useOutletContext<OutletContext>();
 
   const { connectWallet } = useMetamask(setSigner);
 
@@ -23,6 +30,10 @@ function LiquidityPage() {
 
   return (
     <Flex direction="column" spaceY={4} mt={8}>
+      <CurrentLiquidity
+        signer={signer}
+        liquidityPoolContract={liquidityPoolContract}
+      />
       <ApproveToken
         tokenName="Token A"
         signer={signer}
@@ -32,6 +43,10 @@ function LiquidityPage() {
         tokenName="Token B"
         signer={signer}
         tokenContract={tokenBContract}
+      />
+      <AddLiquidity
+        signer={signer}
+        liquidityPoolContract={liquidityPoolContract}
       />
     </Flex>
   );
